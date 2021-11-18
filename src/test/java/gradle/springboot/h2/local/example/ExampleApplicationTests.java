685d90c0034e0173d9fa93c183ca43872c668fbd
Sebastijan Grabar <s.grabar@reply.de>
2021-11-18 14:58:21 +0100

    Initial

---
+++ src/test/java/gradle/springboot/h2/local/example/ExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package gradle.springboot.h2.local.example;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
